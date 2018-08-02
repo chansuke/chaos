ef643a5d092e1104bbbe35a316634936bce3aafe
chansuke <chansuke@georepublic.de>
2018-08-03 01:58:20 +0900

    boot image

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_std]
 #![no_main]
 
+extern crate bootloader_precompiled;
+
 use core::panic::PanicInfo;
 
 #[panic_implementation]
@@ -10,7 +12,18 @@ pub fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
