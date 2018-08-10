570894c54a1a9b706d9cd891e4295341fd2dee33
chansuke <chansuke@georepublic.de>
2018-08-10 17:49:22 +0900

    enable to run QEMU in the background and also shutdown automatically

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@
 extern crate lazy_static;
 #[cfg(test)]
 extern crate array_init;
+extern crate bootloader_precompiled;
 extern crate spin;
 extern crate std;
-
-extern crate bootloader_precompiled;
-extern crate volatile;
-
 extern crate uart_16550;
+extern crate volatile;
+extern crate x86_64;
 
 #[cfg_attr(test, allow(unused_imports))]
 use core::panic::PanicInfo;
@@ -29,6 +28,10 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     serial_println!("Hello Host{}", "!");
 
+    unsafe {
+        exit_qemu();
+    }
+
     loop {}
 }
 
@@ -38,4 +41,11 @@ pub extern "C" fn _start() -> ! {
 pub fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
 }
